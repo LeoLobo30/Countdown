9711a15c001b54be169c5403e3b7fc17d52233b8
Leonardo Lobo <leonardogomeslobo@gmail.com>
2022-03-22 20:32:41 -0300

    concluded struct of countDown

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const timeStart = 1 * 24 * 60 * 60; //day * hr * min * sec
-
 const countDown = document.querySelector("div.countDown");
 
-countDown.innerHTML += `
-  <span>00</span>
-  <span>:</span>
-  <span>00</span>
-  <span>:</span>
-  <span>00</span>
-  <span>:</span>
-  <span>00</span>
-`;
+let timeStart = 2 * 24 * 60 * 60; //day * hr * min * sec
 
+function timeConfigAndShow() {
+  let days = Math.floor(timeStart / (24 * 60 * 60));
+  let hours = Math.floor(timeStart / (60 * 60));
+  let minutes = Math.floor((timeStart % (60 * 60)) / 60);
+  let seconds = timeStart % 60;
+
+  countDown.innerHTML = `
+  <span class="menu">Dias</span><span></span><span class="menu">Horas</span><span></span><span class="menu">Minutos</span><span></span><span class="menu">Segundos</span>
+  <span>${days}</span>
+  <span>:</span>
+  <span>${hours}</span>
+  <span>:</span>
+  <span>${minutes}</span>
+  <span>:</span>
+  <span>${seconds}</span>
+`;
+}
+
+function timeReduction() {
+  if (timeStart > 0) {
+    timeConfigAndShow();
+    --timeStart;
+  } else {
+    timeConfigAndShow();
+    clearInterval(setinterval);
+  }
+}
+
+const setinterval = setInterval(timeReduction, 1000);
