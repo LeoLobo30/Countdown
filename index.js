ba12ee85496d5621896608e6458a200fc7d6bcdf
Leonardo Lobo <leonardogomeslobo@gmail.com>
2022-03-22 22:53:22 -0300

    ajust formation and file index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const countDown = document.querySelector("div.countDown");
+const countDown        = document.querySelector("div.countDown");
+const secondsToDays    = 24 * 60 * 60;
+const secondsToHours   = 60 * 60;
+const secondsToMinutes = 60;
 
-let timeStart = 2 * 24 * 60 * 60; //day * hr * min * sec
+let timeStart = 10 * 24 * 60 * 60; //day * hr * min * sec
+let days, hours, minutes, seconds;
+
+const interval = setInterval(timeReduction, 1000);
 
 function timeConfigAndShow() {
-  let days = Math.floor(timeStart / (24 * 60 * 60));
-  let hours = Math.floor(timeStart / (60 * 60));
-  let minutes = Math.floor((timeStart % (60 * 60)) / 60);
-  let seconds = timeStart % 60;
+  days    = Math.floor(timeStart / secondsToDays);
+  hours   = Math.floor(timeStart % secondsToDays / secondsToHours);
+  minutes = Math.floor((timeStart % secondsToHours) / secondsToMinutes);
+  seconds = timeStart % secondsToMinutes;
 
   countDown.innerHTML = `
   <span class="menu">Dias</span><span></span><span class="menu">Horas</span><span></span><span class="menu">Minutos</span><span></span><span class="menu">Segundos</span>
-  <span>${days}</span>
+  <span>${String(days).padStart(2,"00")}</span>
   <span>:</span>
-  <span>${hours}</span>
+  <span>${String(hours).padStart(2,"00")}</span>
   <span>:</span>
-  <span>${minutes}</span>
+  <span>${String(minutes).padStart(2,"00")}</span>
   <span>:</span>
-  <span>${seconds}</span>
-`;
+  <span>${String(seconds).padStart(2,"00")}</span>`;
 }
 
 function timeReduction() {
@@ -26,8 +31,6 @@ function timeReduction() {
     --timeStart;
   } else {
     timeConfigAndShow();
-    clearInterval(setinterval);
+    clearInterval(interval);
   }
 }
-
-const setinterval = setInterval(timeReduction, 1000);
